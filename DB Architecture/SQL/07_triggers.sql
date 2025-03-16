fba2f66187c7b68a530e8a98224e5fa24bdc405b
Burhanuddin-Ezzy <burhanezzy8@gmail.com>
2025-03-16 18:37:54 -0400

    CREATE OR REPLACE FUNCTION
    CREATE OR REPLACE FUNCTION check_high_error_rate() RETURNS TRIGGER AS $$

--- DB Architecture/SQL/07_triggers.sql
+++ DB Architecture/SQL/07_triggers.sql
@@ -11,3 +11,17 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER trigger_high_cpu
 BEFORE INSERT OR UPDATE ON server_metrics
 FOR EACH ROW EXECUTE FUNCTION check_high_cpu();
+
+
+CREATE OR REPLACE FUNCTION check_high_error_rate() RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.error_rate > 5.00 THEN
+        RAISE NOTICE 'Warning: High error rate detected for server %!', NEW.server_id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER error_rate_check
+BEFORE INSERT OR UPDATE ON aggregated_metrics
+FOR EACH ROW EXECUTE FUNCTION check_high_error_rate();
