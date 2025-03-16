7a6ed68a01c3adc5843788b29f540e29eaf08126
Burhanuddin-Ezzy <burhanezzy8@gmail.com>
2025-03-16 18:37:05 -0400

    CREATE VIEW avg_metrics_per_region

--- DB Architecture/SQL/05_views.sql
+++ DB Architecture/SQL/05_views.sql
@@ -2,3 +2,15 @@ CREATE VIEW recent_server_metrics AS
 SELECT server_id, region, timestamp, cpu_usage, memory_usage, disk_usage_percent
 FROM server_metrics
 WHERE timestamp >= NOW() - INTERVAL '1 day';
+
+
+CREATE VIEW avg_metrics_per_region AS
+SELECT 
+    region, 
+    AVG(hourly_avg_cpu_usage) AS avg_cpu, 
+    AVG(hourly_avg_memory_usage) AS avg_memory, 
+    AVG(average_response_time) AS avg_response_time
+FROM aggregated_metrics
+GROUP BY region;
+
+
