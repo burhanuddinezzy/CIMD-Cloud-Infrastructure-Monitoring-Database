b438af1e83df3659c0f7f70780423be310cc5965
Burhanuddin-Ezzy <burhanezzy8@gmail.com>
2025-03-16 17:29:49 -0400

    Create 04_indexes.sql
    To create indexes later on to speed up searching using different columns

---
+++ DB Architecture/SQL/04_indexes.sql
@@ -0,0 +1,3 @@
+CREATE INDEX idx_server_metrics_region ON server_metrics(region);
+CREATE INDEX idx_server_metrics_cpu_usage ON server_metrics(cpu_usage);
+CREATE INDEX idx_server_metrics_memory_usage ON server_metrics(memory_usage);
