28656aea2df39f112364defe24a0dd18f3a74e78
Burhanuddin-Ezzy <burhanezzy8@gmail.com>
2025-03-16 18:36:18 -0400

    CREATE INDEX
    CREATE INDEX idx_aggregated_metrics_region ON aggregated_metrics(region);

--- DB Architecture/SQL/04_indexes.sql
+++ DB Architecture/SQL/04_indexes.sql
@@ -1,3 +1,7 @@
 CREATE INDEX idx_server_metrics_region ON server_metrics(region);
 CREATE INDEX idx_server_metrics_cpu_usage ON server_metrics(cpu_usage);
 CREATE INDEX idx_server_metrics_memory_usage ON server_metrics(memory_usage);
+
+
+CREATE INDEX idx_aggregated_metrics_region ON aggregated_metrics(region);
+CREATE INDEX idx_aggregated_metrics_timestamp ON aggregated_metrics(timestamp);
