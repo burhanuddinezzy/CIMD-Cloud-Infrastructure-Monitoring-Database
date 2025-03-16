cabda9a2368c1ecd7c31952198d7f5c8629f6747
Burhanuddin-Ezzy <burhanezzy8@gmail.com>
2025-03-16 18:35:22 -0400

    ALTER TABLE aggregated_metrics

--- DB Architecture/SQL/03_alter_tables.sql
+++ DB Architecture/SQL/03_alter_tables.sql
@@ -1,2 +1,5 @@
 ALTER TABLE server_metrics ADD COLUMN io_wait FLOAT NULL;
 ALTER TABLE server_metrics DROP COLUMN error_count;
+
+
+ALTER TABLE aggregated_metrics ADD COLUMN peak_memory_usage DECIMAL(5,2);
