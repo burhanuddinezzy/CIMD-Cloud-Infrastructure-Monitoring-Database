b4d66541eb867c5dff5b7b24a28513d599fe37aa
Burhanuddin-Ezzy <burhanezzy8@gmail.com>
2025-03-18 22:34:08 -0400

    Update 04_indexes.sql
    CREATE INDEX idx_alert_history_server

--- DB Architecture/SQL/04_indexes.sql
+++ DB Architecture/SQL/04_indexes.sql
@@ -8,6 +8,6 @@ CREATE INDEX idx_aggregated_metrics_timestamp ON aggregated_metrics(timestamp);
 
 
 -- 04_indexes.sql - Performance Optimization
-CREATE INDEX idx_alert_history_server ON alert_history(server_id);
+CREATE INDEX idx_alert_history_server ON alert_history(server_id); 
 CREATE INDEX idx_alert_history_status ON alert_history(alert_status);
 CREATE INDEX idx_alert_history_severity ON alert_history(alert_severity);
