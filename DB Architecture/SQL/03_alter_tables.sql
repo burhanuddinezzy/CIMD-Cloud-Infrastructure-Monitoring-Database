e5f46678c622d53620ee8f4bbeeae87035bb580b
Burhanuddin-Ezzy <burhanezzy8@gmail.com>
2025-03-18 22:32:43 -0400

    Update 03_alter_tables.sql
    ALTER TABLE alert_history

--- DB Architecture/SQL/03_alter_tables.sql
+++ DB Architecture/SQL/03_alter_tables.sql
@@ -3,3 +3,7 @@ ALTER TABLE server_metrics DROP COLUMN error_count;
 
 
 ALTER TABLE aggregated_metrics ADD COLUMN peak_memory_usage DECIMAL(5,2);
+
+
+-- 03_alter_tables.sql - Modify Table Structure
+ALTER TABLE alert_history ADD COLUMN response_time INTERVAL;
