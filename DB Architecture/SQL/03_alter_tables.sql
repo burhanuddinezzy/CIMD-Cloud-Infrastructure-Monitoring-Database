43abcf8dca67f341421aefd9007c510dd243f597
Burhanuddin-Ezzy <burhanezzy8@gmail.com>
2025-03-18 22:56:27 -0400

    Update 03_alter_tables.sql
    ALTER TABLE cost_data

--- DB Architecture/SQL/03_alter_tables.sql
+++ DB Architecture/SQL/03_alter_tables.sql
@@ -17,3 +17,7 @@ ALTER TABLE alert_configuration ADD COLUMN notification_channel VARCHAR(50);
 
 -- 03_alter_tables.sql - Modifications (if any)
 ALTER TABLE application_logs ADD COLUMN response_time_ms INTEGER;
+
+
+-- 03_alter_tables.sql - Modifications (if any)
+ALTER TABLE cost_data ADD COLUMN additional_notes TEXT;
